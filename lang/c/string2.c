7fea0e3c58a8c045f9e87bd094542a92f69398b5
Larkin.Huang <huangchao1317@163.com>
2017-04-08 05:47:32 -0700

    lang:c: modify string memory address cmp
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/c/string2.c
+++ lang/c/string2.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
+const char *a="This is good.";
+const char *b="This is good.";
+
+void check(const char *x,const char *y) {
+    if (x==y) printf("same\n");
+    else printf("not same\n");
+}
+
+int main() {
 	char s[]="test func.";
 	char t[]="test func.";
 
-	if (s==t) printf("same\n");
-	else printf("not same\n");
+	check(a,b);
+	check(s,t);
 
 	return 0;
 }
