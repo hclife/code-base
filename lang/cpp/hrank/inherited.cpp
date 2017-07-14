96c6266c1dfa0f4a1db3d114477da5be13829ece
Larkin.Huang <mafia.larkin@gmail.com>
2017-07-14 14:03:47 +0800

    lang:cpp: import more class and inherited code
    Signed-off-by: Larkin.Huang <mafia.larkin@gmail.com>

---
+++ lang/cpp/hrank/inherited.cpp
@@ -0,0 +1,54 @@
+//Inherited code
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+/* Define the exception here */
+class BadLengthException: public exception {
+private:
+    string s;
+public:
+    BadLengthException(int n) {
+        stringstream ss;
+        ss<<n;ss>>s;
+    }
+    virtual const char *what() const throw() {
+        return s.c_str();
+    }
+};
+
+
+bool checkUsername(string username) {
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5) {
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n-1; i++) {
+		if(username[i] == 'w' && username[i+1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		string username;
+		cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if(isValid) {
+				cout << "Valid" << '\n';
+			} else {
+				cout << "Invalid" << '\n';
+			}
+		} catch (BadLengthException e) {
+			cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
+}
