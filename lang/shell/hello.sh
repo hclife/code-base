7de1111ce2b4d500008af1bc41b91a7519763e6b
Larkin.Huang <huangchao1317@163.com>
2017-07-14 23:47:46 -0700

    lang: import helloworld for pps
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/shell/hello.sh
+++ lang/shell/hello.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # program:
-#	This program shows "Hello world!" onto screen.
+# This program shows hello world onto screen.
 # History:
 # 2011/05/20/	Larkin 	Firse case
 #PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 #export PATH
-echo -e "Hello world!\a\n"
+echo -e "Hello world, Bash Shell!\a\n"
 exit 0
