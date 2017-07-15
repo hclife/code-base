33de8e6ff5d2b56bd4aee489a9a082ccc97495fc
Larkin.Huang <huangchao1317@163.com>
2017-07-14 22:25:12 -0700

    lang:c: modify memory test code
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/c/mem.c
+++ lang/c/mem.c
@@ -6,10 +6,10 @@ int main()
 	int a[4]={0};int i;
 	for(i=0;i<4;++i) printf("%d ",a[i]);
 	putchar('\n');
-	memset(a,1,1);
+	memset(a,1,4);
 	//memset(a,0xFF,sizeof(a));
-	printf("sizeof(a)=%u\n",sizeof(a));
-	printf("sizeof(int)=%u\n",sizeof(int));
+	printf("sizeof(a)=%lu\n",sizeof(a));
+	printf("sizeof(int)=%lu\n",sizeof(int));
 	printf("a[%d]=%d=0x%08x\n",0,a[0],a[0]);
 	//for(int i=0;i<4;++i) printf("%d ",a[i]);
 	//putchar('\n');
