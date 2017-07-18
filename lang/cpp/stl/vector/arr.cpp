795ea104055c8e12dca175289c278f937d48ad88
Larkin.Huang <huangchao1317@163.com>
2017-07-17 18:52:01 -0700

    lang:cpp:stl: modify arr.cpp and itor.cpp of vector
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/cpp/stl/vector/arr.cpp
+++ lang/cpp/stl/vector/arr.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {  
-	vector<int> array;  
-	array.push_back(1);  
-	array.push_back(2);  
-	array.push_back(3);  
+int main() {
+	vector<int> array;
+	array.push_back(1);
+	array.push_back(2);
+	array.push_back(3);
 	//vector<int>::size_type i;
-	//for(i=array.size()-1; i>=0; --i)
-	for (int i=array.size()-1;i>=0;--i) {  
+	//for(i=array.size()-1;i>=0;--i)
+	for (int i=array.size()-1;i>=0;--i) {
 		cout << array[i] << endl;
 	}
-	return 0;  
+	return 0;
 }
--- lang/cpp/stl/vector/itor.cpp
+++ lang/cpp/stl/vector/itor.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {  
+int main() {
 	typedef vector<int> IntArray;
 	IntArray array;
 	array.push_back(1);
@@ -10,9 +10,9 @@ int main() {
 	array.push_back(2);
 	array.push_back(3);
 	IntArray::iterator itor;
-	for(itor=array.begin();itor!=array.end();++itor) {  
+	for(itor=array.begin();itor!=array.end();++itor) {
 		if(2 == *itor) array.erase(itor--);
-	}  
+	}
 	for (int i=0;i<array.size();++i) {
 		cout<<array[i]<<endl;
 	}
