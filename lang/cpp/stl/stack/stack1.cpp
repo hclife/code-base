c024870c0a80366791fde62e6bd4fabdf0b7ec0a
Larkin.Huang <huangchao1317@163.com>
2017-07-26 06:52:59 -0700

    lang:cpp:stl:stack: file format from dos to unix
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/cpp/stl/stack/stack1.cpp
+++ lang/cpp/stl/stack/stack1.cpp
@@ -3,18 +3,15 @@
 #include <stack>          // std::stack
 
 int main() {
-  std::stack<int> mystack;
   int sum(0);
-
+  std::stack<int> mystack;
   for (int i=1;i<=10;i++) mystack.push(i);
 
   std::cout << "Popping out elements...";
-  while (!mystack.empty())
-  {
+  while (!mystack.empty()) {
      sum += mystack.top();
      mystack.pop();
   }
-
   std::cout << "total: " << sum << '\n';
 
   return 0;
