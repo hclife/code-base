1fa8988f2f8e8a9163d4eee4405bf45a454af432
Larkin.Huang <mafia.larkin@gmail.com>
2017-12-11 14:51:35 +0800

    algo: import basic program sample.cpp
    Signed-off-by: Larkin.Huang <mafia.larkin@gmail.com>

---
+++ algo/sample.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <set>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define rint		register int
+#define to_upper(c)	('A'+(c)-'a')
+#define to_lower(c)	('a'+(c)-'A')
+#define is_digit(c)	((c)>='0' && (c)<='9')
+#define is_lower(c)	((c)>='a' && (c)<='z')
+#define is_upper(c)	((c)>='A' && (c)<='Z')
+#define is_alpha(c)	(is_lower(c) || is_upper(c))
+
+int main() {
+	return 0;
+}
