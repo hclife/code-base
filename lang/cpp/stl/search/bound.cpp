4df3f28a3ddeeb7274da75bfa079fa304c9fb7f0
Larkin.Huang <mafia.larkin@gmail.com>
2018-01-09 14:53:30 +0800

    lang:cpp:stl:search: modify lower/upper bound
    Signed-off-by: Larkin.Huang <mafia.larkin@gmail.com>

--- lang/cpp/stl/search/bound.cpp
+++ lang/cpp/stl/search/bound.cpp
@@ -1,23 +1,32 @@
 // lower_bound/upper_bound example
 #include <iostream>     // std::cout
-#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
+#include <algorithm>    // std::lower_bound,std::upper_bound,std::sort
 #include <vector>       // std::vector
+using namespace std;
 
-int main () {
+int main() {
 	int myints[] = {10,20,30,30,20,10,10,20};
-	std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
-	std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
 
+	// 10 20 30 30 20 10 10 20
+	std::vector<int> v(myints,myints+8);
+
+	// 10 10 10 20 20 20 30 30
+	std::sort(v.begin(),v.end());
+
+	for (size_t i=0;i<v.size();++i) cout<<v[i]<<' ';
+	cout<<endl;
+
+#if 1
 	while (true) {
 		int val;std::cin>>val;
 		std::vector<int>::iterator low,up;
-		low=std::lower_bound (v.begin(), v.end(), val);
-		up=std::upper_bound (v.begin(), v.end(), val);
-		std::cout << "lower_bound at position " << (low- v.begin()) << '\n';
-		std::cout << "upper_bound at position " << (up - v.begin()) << '\n';
+		low=std::lower_bound(v.begin(),v.end(),val);
+		up=std::upper_bound(v.begin(),v.end(),val);
+		cout<<"lower_bound at position "<<(low-v.begin())<<'\n';
+		cout<<"upper_bound at position "<<(up-v.begin())<<'\n';
 	}
 
-#if 0
+#else
 	while (1) {
 		u64 x;cin>>x;
 		vector<u64>::iterator low,up;
