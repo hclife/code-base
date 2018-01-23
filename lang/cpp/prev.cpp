e8712205d3c30d47422494a314a30d924ccf77e9
Larkin.Huang <mafia.larkin@gmail.com>
2018-01-23 13:51:10 +0800

    lang:cpp: import iterator operation samples
    Signed-off-by: Larkin.Huang <mafia.larkin@gmail.com>

---
+++ lang/cpp/prev.cpp
@@ -0,0 +1,13 @@
+// prev example
+#include <iostream>     // std::cout
+#include <iterator>     // std::next
+#include <list>         // std::list
+#include <algorithm>    // std::for_each
+
+int main () {
+  std::list<int> mylist;
+  for (int i=0; i<10; i++) mylist.push_back (i*10);
+  std::cout << "The last element is " << *std::prev(mylist.end()) << '\n';
+
+  return 0;
+}
