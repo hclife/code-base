0327e17358b2c588aa8ad990ff167ecc3ef3a6cf
Larkin.Huang <huangchao1317@163.com>
2018-05-29 22:03:52 +0800

    lang:python: expand tuple data structures
    Signed-off-by: Larkin.Huang <huangchao1317@163.com>

--- lang/python/practices/ds_using_tuple.py
+++ lang/python/practices/ds_using_tuple.py
@@ -8,3 +8,14 @@ print('All animals in new zoo are',new_zoo)
 print('Animals brought from old zoo are',new_zoo[2])
 print('Last animal brought from old zoo is',new_zoo[2][2])
 print('Number of animals in new zoo is',len(new_zoo)-1+len(new_zoo[2]))
+
+empty=()
+print(empty)
+print(len(empty))
+singleton='hello',
+print(singleton)
+print(len(singleton))
+t=123,321,'hello' # tuple packing
+print(t)
+x,y,z=t # sequence unpacking
+print('x =',x,'y =',y,'z =',z)
